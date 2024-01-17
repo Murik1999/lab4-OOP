4ec1874a32786b5012cca0962779cbe251cd2e02
Murik1999 <98152309+Murik1999@users.noreply.github.com>
2024-01-17 16:02:00 +0300

    Add files via upload

--- main.java
+++ main.java
@@ -1,6 +1,5 @@
 public static void main(String[] args) {
-    // Код, который будет выполнен при запуске программы
-    SwingUtilities.invokeLater(new Runnable() {
+    SwingUtilities.strings(new Runnable() {
         @Override
         public void run() {
             stroki app = new stroki();
